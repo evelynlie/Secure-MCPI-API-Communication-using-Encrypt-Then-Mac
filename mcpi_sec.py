f29171a29aa27abf1a53dd00ba91606c8fa3842f
EdwardLimPadmajaya <edwardpadmajaya@gmail.com>
2023-05-31 13:27:20 +1000

    update

--- mcpi_sec.py
+++ mcpi_sec.py
@@ -8,20 +8,35 @@ from mcpi.minecraft import Minecraft
 
 
 #HMAC EXAMPLE
-#The timestamp is combined with the file's data. The timestamp is unique for each second, thus giving a different HMAC every second. 
-#Please be aware that by doing this, you are no longer verifying the integrity of just the file, but rather the file combined with the timestamp.
 def create_hmac_for_file(file_path, secret_key):
     with open(file_path, 'rb') as f:
         file_data = f.read()
 
-    timestamp = str(time.time()).encode()
-    data = file_data + timestamp
-
-    hmac_obj = hmac.new(secret_key.encode(), data, hashlib.sha256)
+    hmac_obj = hmac.new(secret_key.encode(), file_data, hashlib.sha256)
     return hmac_obj.hexdigest()
+
+# You can use the function like this:
+secret_key = 'mysecretkey'
+file_path = "C:/Users/Edward/Downloads/testing.txt" # replace with your file path
+hmac_result = create_hmac_for_file(file_path, secret_key)
+
+print(f'HMAC for the file from the sender: {hmac_result}')
+def verify_hmac_for_file(file_path, received_hmac, secret_key):
+    with open(file_path, 'rb') as f:
+        file_data = f.read()
+
+    hmac_obj = hmac.new(secret_key.encode(), file_data, hashlib.sha256)
+    calculated_hmac = hmac_obj.hexdigest()
+
+    return hmac.compare_digest(calculated_hmac, received_hmac)
 
 # Use the function:
 secret_key = 'mysecretkey'
 file_path = "C:/Users/Edward/Downloads/testing.txt" # replace with your file path
-hmac_result = create_hmac_for_file(file_path, secret_key)
-print(f'HMAC for the file: {hmac_result}')
+received_hmac = hmac_result  # replace with the received HMAC
+verification_result = verify_hmac_for_file(file_path, received_hmac, secret_key)
+print(f'HMAC for the file from the receiver: {received_hmac}')
+if verification_result:
+    print('The message is authentic and unaltered.')
+else:
+    print('The message is not authentic or has been tampered with.')
