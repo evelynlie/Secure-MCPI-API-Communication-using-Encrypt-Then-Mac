0617b71f885b81a5ee0aa8465bf3f5a262f12b08
Evelyn Lie <s3951140@student.rmit.edu.au>
2023-06-09 14:13:49 +1000

    Added address and port

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from mcpi_elci_master.mcpi import minecraft
 #from mcpi import minecraft
 
-mc = minecraft.Minecraft.create(address="localhost", port=4712)
+mc = minecraft.Minecraft.create(address="localhost", port=4711)
 
 #[print(bl) for bl in mc.getBlocks(0,0,0,10,10,10)]
 
